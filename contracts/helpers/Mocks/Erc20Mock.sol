f650de06bddcec4350fc507a9f72317d79afbfc5
Sinicki Bogdan <sinicki.bogdan@gmail.com>
2023-05-12 06:53:24 +0200

    Add decimals to constructor

--- contracts/helpers/Mocks/Erc20Mock.sol
+++ contracts/helpers/Mocks/Erc20Mock.sol
@@ -5,7 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract Erc20Mock is ERC20, ERC20Burnable {
-    constructor(string memory name, string memory symbol, uint256 amount) ERC20(name, symbol) {
+    uint8 private _decimals;
+
+    constructor(string memory name, string memory symbol, uint256 amount, uint8 decimals_) ERC20(name, symbol) {
         _mint(msg.sender, amount);
+        _decimals = decimals_;
     }    
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
 }
